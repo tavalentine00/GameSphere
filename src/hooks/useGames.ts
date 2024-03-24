140a6bd8f42f54b657088b7cfe700211555f1c4f
tavalentine00 <tavalentinecs@gmail.com>
2024-03-24 13:50:17 -0700

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,6 +13,7 @@ export interface Game {
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   description: string;
+  rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
